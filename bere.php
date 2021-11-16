ff596c16155769f79e8155c11c857c4bd9b72692
Alex Bordei <alex@whiz.ro>
2021-11-16 20:43:46 +0200

    Cleaning up

--- bere.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Bere extends Sticla {
-	protected $denumire_produs = "Golden Braw";
-	protected $culoare = "maroniu";
-	protected $volum_alcool = 5;
-
-}
--- suc_de_portocale.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Suc_de_portocale extends Sticla {
-	protected $denumire_produs = "Suc de portocale";
-
-	public function toarna(){
-		echo "de acum faci cu totul altceva";
-	}
-}
